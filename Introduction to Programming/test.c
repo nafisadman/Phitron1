a0dd3690805fe97cd48f219158dc51d8aaad2097
Nafi Sadman Khan <sadmannafi789@gmail.com>
2026-02-10 15:35:51 +0600

    first commit

---
+++ Introduction to Programming/test.c
@@ -0,0 +1,8 @@
+//code-1 => Paste it and run the code; you will see the result.
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("Hello world! a = %d", a);
+    return 0;
+}
